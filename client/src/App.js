cc5bcceb9c5a1541cdf039534787475140f4eb3d
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2020-04-10 21:15:47 -0500

    add bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Finn</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div className="container">
+                <hr/>
+                <div className="jumbotron">
+                    <h1>Finn</h1>
+                    <p className="lead">Personal Financial Tracker.</p>
+                </div>
+                <hr/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
