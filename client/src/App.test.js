e72b8b7ac490eeb13cb9f7d435e3dbde83dcf355
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2020-04-10 21:22:23 -0500

    update client test part 2

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Finn/i);
+  const linkElement = getByText(/Personal Financial Tracker./i);
   expect(linkElement).toBeInTheDocument();
 });
